6f39d34ccf3473cd5a7a6d0f55f6f7db0fc4b579
李志军 <liamym@163.com>
2017-07-11 16:50:47 +0800

    初次提交

---
+++ app/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, IndexRoute, hashHistory, Router } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import App from './container/App';
+
+import {reducer} from './reducer'
+// import AllMemosRoute from './routes/AllMemosRoute';
+// import TodoRoute from './routes/TodoRoute';
+// import DoingRoute from './routes/DoingRoute';
+// import DoneRoute from './routes/DoneRoute';
+
+import './main.less';
+
+const store = createStore(reducer)
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={hashHistory}>
+            <Route
+                path="/"
+                component={App}
+            >
+                {/*<IndexRoute component={AllMemosRoute} />
+                <Route path="/todo" component={TodoRoute} />
+                <Route path="/doing" component={DoingRoute} />
+                <Route path="/done" component={DoneRoute} />*/}
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
